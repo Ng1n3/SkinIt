3205d294f22f3358efd202afe9e0f652ebaf4c29
ng1n3 <code.with.muyiwa@gmail.com>
2024-06-01 20:22:46 +0100

    hashing passwords

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,6 @@
+import { NextFunction } from "express";
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 export interface UserInput {
   email: string;
@@ -27,6 +29,34 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  let user = this as UserDocument;
+  const salt = await bcrypt
+    .genSalt(Number(process.env.SALT))
+    .catch((error: any) => {
+      throw new Error("error while salting");
+    });
+  const hash = await bcrypt.hash(user.password, salt).catch((error: any) => {
+    throw new Error("Error while hashing password");
+  });
+
+  user.password = hash;
+  next();
+});
+
+userSchema.methods.comparePassword = async function (
+  password: string
+): Promise<boolean> {
+  const user = this as UserDocument;
+
+  try {
+    const isMatch = await bcrypt.compare(password, user.password);
+    return isMatch;
+  } catch (error: any) {
+    return false;
+  }
+};
 
 const userModel = mongoose.model<UserDocument>("users", userSchema);
 
